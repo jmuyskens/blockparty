d9e22112c145e9334510196d351ad8491c15ff9a
jmuyskens <jmuyskens@gmail.com>
2015-10-07 03:15:06 -0400

    kinda working basically

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, make_response, url_for, redirect
+from flask import Flask, request, render_template, make_response, url_for, redirect, jsonify
 from werkzeug import secure_filename
 from pymongo import MongoClient
 from gridfs import GridFS
@@ -17,33 +17,43 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
-@app.route('/file-upload', methods=['POST'])
-def upload():
 
+@app.route('/create', methods=['POST'])
+def create():
 	block = {}
-	filez = []
-	block['files'] = filez
+	block['files'] = []
 	block_id = blocks.insert_one(block).inserted_id
+	print block_id
+	return jsonify(**{'id': str(block_id)})
 
-	file = request.files['file']
 
-	filename = str(block_id) + '/' + secure_filename(file.filename)
+@app.route('/<id>/upload', methods=['POST'])
+def upload(id):
+	files = request.files.getlist('file')
+	file_storage = []
+	for file in files:
+		filename = str(id) + '/' + secure_filename(file.filename)
 
-	oid = fs.put(file, content_type=file.content_type, filename=filename)
+		oid = fs.put(file, content_type=file.content_type, filename=filename)
 
-	filez.append({
-		'name': filename,
-		'id': oid
-	})
+		file_storage.append({
+			'name': filename,
+			'id': str(oid)
+		})
+	print file_storage
+	print id
+	print blocks.find_one({'_id':id})
+	print blocks.find_one_and_update({'_id':id}, {'$set': {'files': file_storage}}, upsert=True)
 
-	blocks.update({'_id':block_id}, {'files': filez})
-	return redirect(url_for('view_block', id=block_id))
+	return jsonify(**{'num': len(file_storage)})
 
 
 @app.route('/<id>')
 def view_block(id):
-	block = blocks.find_one()
+	block = blocks.find_one({'_id':id})
+	print block['files']
 	return render_template('block.html', block=block, files=block['files'])
+
 
 @app.route('/<id>/<filename>')
 def view_file(id, filename):
---
+++ static/drag.js
@@ -0,0 +1,79 @@
+// set up drag drop data
+var dataTextArea = document.getElementById('drop');
+
+dataTextArea.addEventListener("drag", function( event ) {
+
+}, false);
+
+dataTextArea.addEventListener("dragstart", function( event ) {
+  // store a ref. on the dragged elem
+  dragged = event.target;
+  // make it half transparent
+  event.target.style.opacity = .5;
+}, false);
+
+dataTextArea.addEventListener("dragend", function( event ) {
+  // reset the transparency
+  event.target.style.opacity = "";
+}, false);
+
+/* events fired on the drop targets */
+dataTextArea.addEventListener("dragover", function( event ) {
+  // prevent default to allow drop
+  event.preventDefault();
+}, false);
+
+dataTextArea.addEventListener("dragenter", function( event ) {
+  // highlight potential drop target when the draggable element enters it
+  if ( event.target.id == "drop" ) {
+      event.target.className = "dropzone";
+  }
+}, false);
+
+dataTextArea.addEventListener("dragleave", function( event ) {
+  // reset background of potential drop target when the draggable element leaves it
+  if ( event.target.id == "drop" ) {
+      event.target.className = "";
+  }
+
+}, false);
+
+dataTextArea.addEventListener("drop", function( event ) {
+  // prevent default action (open as link for some elements)
+  event.preventDefault();
+  // move dragged elem to the selected drop target
+  if ( event.target.className == "dropzone" ) {
+      event.target.style.background = "";
+  }
+
+  var dt = event.dataTransfer;
+  var files = dt.files;
+  var reader = new FileReader();
+
+  fd = new FormData();
+
+  for (var i = 0; i < files.length; i++) {
+  	fd.append('file', files[i]);
+  }
+
+  var createAjax = new XMLHttpRequest();
+  createAjax.open('POST', 'create', true);
+
+  var uploadAjax = new XMLHttpRequest();
+  var id;
+
+  createAjax.addEventListener('load', function(e) {
+  	id = JSON.parse(createAjax.responseText).id;
+
+  	uploadAjax.open('POST', id + '/upload', true);
+  	uploadAjax.send(fd);
+  });
+
+  uploadAjax.addEventListener('load', function(e) {
+  	window.location.href += id;
+  });
+
+  createAjax.send();
+
+
+}, false);
